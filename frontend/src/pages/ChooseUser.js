27c32316bff000895252339f9b7e29e56de44336
Djezzar Moncef <djezzarmonsef@gmail.com>
2024-11-10 22:24:55 +0100

    refactor: update Homepage layout and styles, enhance CSS structure, and optimize package-lock.json

--- frontend/src/pages/ChooseUser.js
+++ frontend/src/pages/ChooseUser.js
@@ -15,114 +15,93 @@ import { loginUser } from '../redux/userRelated/userHandle';
 import Popup from '../components/Popup';
 
 const ChooseUser = ({ visitor }) => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const password = "zxc"
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const password = "zxc";
 
-  const { status, currentUser, currentRole } = useSelector(state => state.user);;
+  const { status, currentUser, currentRole } = useSelector(state => state.user);
 
-  const [loader, setLoader] = useState(false)
+  const [loader, setLoader] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
   const [message, setMessage] = useState("");
 
   const navigateHandler = (user) => {
     if (user === "Admin") {
       if (visitor === "guest") {
-        const email = "yogendra@12"
-        const fields = { email, password }
-        setLoader(true)
-        dispatch(loginUser(fields, user))
-      }
-      else {
+        const email = "yogendra@12";
+        const fields = { email, password };
+        setLoader(true);
+        dispatch(loginUser(fields, user));
+      } else {
         navigate('/Adminlogin');
       }
-    }
-
-    else if (user === "Student") {
+    } else if (user === "Student") {
       if (visitor === "guest") {
-        const rollNum = "1"
-        const studentName = "Dipesh Awasthi"
-        const fields = { rollNum, studentName, password }
-        setLoader(true)
-        dispatch(loginUser(fields, user))
-      }
-      else {
+        const rollNum = "1";
+        const studentName = "Dipesh Awasthi";
+        const fields = { rollNum, studentName, password };
+        setLoader(true);
+        dispatch(loginUser(fields, user));
+      } else {
         navigate('/Studentlogin');
       }
-    }
-
-    else if (user === "Teacher") {
+    } else if (user === "Teacher") {
       if (visitor === "guest") {
-        const email = "tony@12"
-        const fields = { email, password }
-        setLoader(true)
-        dispatch(loginUser(fields, user))
-      }
-      else {
+        const email = "tony@12";
+        const fields = { email, password };
+        setLoader(true);
+        dispatch(loginUser(fields, user));
+      } else {
         navigate('/Teacherlogin');
       }
     }
-  }
+  };
 
   useEffect(() => {
     if (status === 'success' || currentUser !== null) {
       if (currentRole === 'Admin') {
         navigate('/Admin/dashboard');
-      }
-      else if (currentRole === 'Student') {
+      } else if (currentRole === 'Student') {
         navigate('/Student/dashboard');
       } else if (currentRole === 'Teacher') {
         navigate('/Teacher/dashboard');
       }
-    }
-    else if (status === 'error') {
-      setLoader(false)
-      setMessage("Network Error")
-      setShowPopup(true)
+    } else if (status === 'error') {
+      setLoader(false);
+      setMessage("Network Error");
+      setShowPopup(true);
     }
   }, [status, currentRole, navigate, currentUser]);
 
   return (
     <StyledContainer>
-      <Container>
-        <Grid container spacing={2} justifyContent="center">
+      <Container maxWidth="md">
+        <Grid container spacing={3} justifyContent="center">
           <Grid item xs={12} sm={6} md={4}>
-            <div onClick={() => navigateHandler("Admin")}>
-              <StyledPaper elevation={3}>
-                <Box mb={2}>
-                  <AccountCircle fontSize="large" />
-                </Box>
-                <StyledTypography>
-                  Admin
-                </StyledTypography>
-                Login as an administrator to access the dashboard to manage app data.
-              </StyledPaper>
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <StyledPaper elevation={3}>
-              <div onClick={() => navigateHandler("Student")}>
-                <Box mb={2}>
-                  <School fontSize="large" />
-                </Box>
-                <StyledTypography>
-                  Student
-                </StyledTypography>
-                Login as a student to explore course materials and assignments.
-              </div>
+            <StyledPaper elevation={3} onClick={() => navigateHandler("Admin")}>
+              <Box mb={2}>
+                <AccountCircle fontSize="large" />
+              </Box>
+              <StyledTypography>Admin</StyledTypography>
+              Login as an administrator to access the dashboard to manage app data.
             </StyledPaper>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <StyledPaper elevation={3}>
-              <div onClick={() => navigateHandler("Teacher")}>
-                <Box mb={2}>
-                  <Group fontSize="large" />
-                </Box>
-                <StyledTypography>
-                  Teacher
-                </StyledTypography>
-                Login as a teacher to create courses, assignments, and track student progress.
-              </div>
+            <StyledPaper elevation={3} onClick={() => navigateHandler("Student")}>
+              <Box mb={2}>
+                <School fontSize="large" />
+              </Box>
+              <StyledTypography>Student</StyledTypography>
+              Login as a student to explore course materials and assignments.
+            </StyledPaper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <StyledPaper elevation={3} onClick={() => navigateHandler("Teacher")}>
+              <Box mb={2}>
+                <Group fontSize="large" />
+              </Box>
+              <StyledTypography>Teacher</StyledTypography>
+              Login as a teacher to create courses, assignments, and track student progress.
             </StyledPaper>
           </Grid>
         </Grid>
@@ -141,27 +120,41 @@ const ChooseUser = ({ visitor }) => {
 
 export default ChooseUser;
 
+// Styled Components
+
 const StyledContainer = styled.div`
-  background: linear-gradient(to bottom, #411d70, #19118b);
-  height: 120vh;
+  background: linear-gradient(135deg, #f0f4f8, #d9e8ff);
+  min-height: 100vh;
   display: flex;
   justify-content: center;
+  align-items: center;
   padding: 2rem;
 `;
 
 const StyledPaper = styled(Paper)`
-  padding: 20px;
+  padding: 24px;
   text-align: center;
-  background-color: #1f1f38;
-  color:rgba(255, 255, 255, 0.6);
-  cursor:pointer;
+  background-color: #ffffff;
+  color: #333;
+  cursor: pointer;
+  border-radius: 12px;
+  transition: background-color 0.3s, box-shadow 0.3s;
+  box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.1);
+  height: 250px; /* Set a fixed height */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
   &:hover {
-    background-color: #2c2c6c;
-    color:white;
+    background-color: #f0f4f8;
+    box-shadow: 0px 15px 35px rgba(0, 0, 0, 0.15);
   }
 `;
 
+
 const StyledTypography = styled.h2`
-  margin-bottom: 10px;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #007bff;
+  margin-bottom: 12px;
 `;
